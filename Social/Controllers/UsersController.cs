aff185de03cc29dac0ebfbab24d2daa6e4a75645
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2025-05-07 19:21:18 +0700

    User Controller
    Creating a user controller for user profile detail

---
+++ Social/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Social_App.Controllers.Base;
+
+namespace Social_App.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Details(string userid)
+        {
+           
+            return View();
+        }
+    }
+}
