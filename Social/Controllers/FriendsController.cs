4392aab63f81da473bb176b80282f2a437a343e9
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2025-05-08 20:24:52 +0700

    friend request

--- Social/Controllers/FriendsController.cs
+++ Social/Controllers/FriendsController.cs
@@ -1,9 +1,10 @@
 ﻿using DataAccess.Services;
 using Microsoft.AspNetCore.Mvc;
+using Social_App.Controllers.Base;
 
 namespace Social_App.Controllers
 {
-    public class FriendsController : Controller
+    public class FriendsController : BaseController
     {
         public readonly IFriendService _friendService;
         public FriendsController(IFriendService friendService)
@@ -13,6 +14,15 @@ namespace Social_App.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendFriendRequest(string receiverId)
+        {
+          var userId = GetUserId();
+            if (string.IsNullOrEmpty(userId)) RedirectToLogin();
+
+            await _friendService.SendRequestAsync(userId, receiverId);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
