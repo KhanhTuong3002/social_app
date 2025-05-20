ee8f425118b4845fba0ab73b2414310026cda2a1
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2025-05-20 18:22:15 +0700

    update register

--- Social/ViewModel/Authentication/RegisterVM.cs
+++ Social/ViewModel/Authentication/RegisterVM.cs
@@ -6,12 +6,12 @@ namespace Social_App.ViewModel.Authentication
     {
         [Required(ErrorMessage ="First Name is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 50 characters.")]
-        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)[a-zA-Z]+$", ErrorMessage = "First Name can only contain letters and cannot contain 'admin'.")]
+        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)(?!.*\s{2,})[\p{L}\s]+$", ErrorMessage = "First Name can only contain letters and cannot contain 'admin'.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Last Name must be between 2 and 50 characters.")]
-        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)[a-zA-Z]+$", ErrorMessage = "First Name can only contain letters and cannot contain 'admin'.")]
+        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)(?!.*\s{2,})[\p{L}\s]+$", ErrorMessage = "First Name can only contain letters and cannot contain 'admin'.")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
--- Social/ViewModel/Settings/ProfileVm.cs
+++ Social/ViewModel/Settings/ProfileVm.cs
@@ -9,7 +9,7 @@ namespace Social_App.ViewModel.Settings
         public string FullName { get; set; }
 
         [Required(ErrorMessage = "UserName is required")]
-        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)[a-zA-Z0-9._-]+$", ErrorMessage = "UserName can only contain letters, numbers, dots (.), dashes (-), underscores (_) and cannot contain the word 'admin'.")]
+        [RegularExpression(@"^(?!.*\b[aA][dD][mM][iI][nN]\b)[a-zA-Z0-9._%+@-]+$", ErrorMessage = "UserName can only contain letters, numbers, dots (.), dashes (-), underscores (_) and cannot contain the word 'admin'.")]
         public string UserName { get; set; }
         public string Bio { get; set; }
     }
